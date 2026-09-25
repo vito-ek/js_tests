2dc22231ef5b6e82da34497e8eb03da0a6c3f294
Vigga Tøt <vios1000@stud.ek.dk>
2026-09-25 09:46:15 +0200

    links added

---
+++ øvelse2/index.js
@@ -0,0 +1,16 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/categories";
+
+const catListeContainer = document.querySelector("#catListeContainer");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    catListeContainer.innerHTML += `
+      <a href="productlist.html?cat=${element.category}"> ${element.category} </a>
+    `;
+  });
+}
